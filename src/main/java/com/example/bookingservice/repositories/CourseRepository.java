74c63e0d509265b08863ead2492c07902afd4408
SShingler <s.shingler@hotmail.co.uk>
2023-04-27 15:41:57 +0100

    add routes

--- src/main/java/com/example/bookingservice/repositories/CourseRepository.java
+++ src/main/java/com/example/bookingservice/repositories/CourseRepository.java
@@ -3,7 +3,12 @@ import com.example.bookingservice.models.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    List<Course> findCoursesByStarRating (int rating);
+    List<Course> findAllByBookingsCustomerId(Long customerId);
 
 }
--- src/main/java/com/example/bookingservice/repositories/CustomerRepository.java
+++ src/main/java/com/example/bookingservice/repositories/CustomerRepository.java
@@ -5,6 +5,12 @@ import com.example.bookingservice.models.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    List<Customer> findAllBookingsCourseID(Long courseId);
+    List<Customer> findAllByTownAndBookingsCourseID(String town, Long courseId);
+
+//    List<Customer>findAllByTownAndAgeGreaterThan
 }
